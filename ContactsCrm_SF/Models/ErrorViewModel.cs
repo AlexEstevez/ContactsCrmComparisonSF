3519f1681da6a54a7ae8ce1e5098d80d57ff2833
Alex Estevez <pit_errio@hotmail.com>
2017-12-09 13:18:39 -0600

    MVC Project Creation This includes the inital project creation.

---
+++ ContactsCrm_SF/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ContactsCrm_SF.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
